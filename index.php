ae6331c5af25f28e0f2bf8764cea453ef95aa1e5
Igor-86 <i.campoli86@gmail.com>
2022-11-28 16:57:56 +0100

    Milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+$leng_pass = $_GET["lengpass"];
+$leng_pass = intval($leng_pass);
+
+$valid_string = "ghugiuygloihBUIBUOHEWQBNC123456789,.-<>!?^*°-_";
+
+$result = generator_password($leng_pass, $valid_string);
+
+function generator_password($leng_pass, $valid_string)
+{
+    $result = "";
+    if ($leng_pass > 0) {
+        for ($i = 0; $i < $leng_pass; $i++) {
+
+            $result .= $valid_string[rand(0, strlen($valid_string)) - 1];
+        }
+        return $result;
+    }
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +33,19 @@
 </head>
 
 <body>
-
+    <main>
+        <form action="index.php" method="GET">
+            <label for="lengpass">Lunghezza</label>
+            <input type="number" name="lengpass" id="lengpass">
+            <div>
+                <button type="submit">Crea</button>
+                <button type="reset">Annulla</button>
+            </div>
+        </form>
+        <div class="resalt">
+            <h2><?php echo $result; ?></h2>
+        </div>
+    </main>
 </body>
 
 </html>

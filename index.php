ce44228555c3acf1f58f589dce289718b298bcc4
igor-86 <i.campoli86@gmail.com>
2022-11-28 22:16:57 +0100

    Style

--- index.php
+++ index.php
@@ -22,20 +22,29 @@ if (!empty($leng_pass)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
 </head>
 
 <body>
     <main>
-        <form action="index.php" method="GET">
-            <label for="lengpass">Lunghezza</label>
-            <input type="number" name="lengpass" id="lengpass">
-            <div>
-                <button type="submit">Crea</button>
-                <button type="reset">Annulla</button>
-            </div>
-            <a href="print.php">vedi</a>
-        </form>
-
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+        <div class="form-cont">
+            <form action="index.php" method="GET" class="form">
+                <div class="input">
+                    <label for="lengpass" class="fs-5">Lunghezza</label>
+                    <input type="number" name="lengpass" id="lengpass" class="mb-3 input-width">
+                </div>
+                <div class="mb-1">
+                    <button class="btn btn-primary" type="submit">Crea</button>
+                    <button class="btn btn-danger" type="reset">Annulla</button>
+                </div>
+                <a class="btn btn-primary" href="print.php">Controlla</a>
+            </form>
+        </div>
     </main>
 </body>
 
--- print.php
+++ print.php
@@ -12,13 +12,17 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password generator</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="resalt">
         <h2>La password che hai generato è:</h2>
         <h2><?php echo $_SESSION["lengpass"]; ?></h2>
+        <a href="index.php" class="btn btn-primary margin-auto">Indietro</a>
     </div>
+
 </body>
 
 </html>
